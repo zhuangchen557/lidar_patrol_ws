2dd624c64206a5d35dab50be907bd8319c8c27c5
sagac1ty0812 <842767563@qq.com>
2026-08-06 10:44:54 +0800

    完成 chassis_node + 修复退出报错

--- src/vehicle_bringup/setup.py
+++ src/vehicle_bringup/setup.py
@@ -1,29 +1,20 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-package_name = 'vehicle_bringup'
+package_name = "vehicle_bringup"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='sagac1ty',
-    maintainer_email='842767563@qq.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "chassis_node = vehicle_bringup.chassis_node:main",
         ],
     },
 )
